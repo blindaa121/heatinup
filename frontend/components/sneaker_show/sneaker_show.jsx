bd9bdd644abfc36a4c63ed4b17917bf9185159fc
Bryan <blindaa121@gmail.com>
2020-05-05 21:18:47 -0700

    added z-index to nav bar to prevent picture bleeding into it

--- frontend/components/sneaker_show/sneaker_show.jsx
+++ frontend/components/sneaker_show/sneaker_show.jsx
@@ -8,6 +8,9 @@ import { Link } from 'react-router-dom';
 class SneakerShow extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            
+        }
     };
 
     componentDidMount() {
