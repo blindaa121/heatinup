d26cf4026262cba0df4d25fae5d5d6d2c63a4e4b
Bryan <blindaa121@gmail.com>
2020-06-05 16:17:15 -0700

    Fix sneaker panel alignment, baseline

--- frontend/components/sneakers/sneakers_index.jsx
+++ frontend/components/sneakers/sneakers_index.jsx
@@ -7,22 +7,36 @@ class SneakerIndex extends React.Component {
         this.state = {
             search: ''
         }
+        this.onChange = this.onChange.bind(this);
     }
 
     componentDidMount() {
         this.props.fetchSneakers()
     }
 
-    render () {
+    onChange(e) {
+        this.setState({ search: e.target.value })
+    }
 
+    render () {
+        const { search } = this.state;
         const { sneakers } = this.props;
+        const { sneaker } = this.props;
+        // debugger
         if (!sneakers) return null;
+
 
         return (
             <div className="outerSneakerdiv">
                 <h1>Shop All</h1>
+                
+                <div className='search-container'>
+                    <i class="fas fa-search fa-3x"></i>
+                    <input className='search-input' label="Search for a sneaker" onChange={this.onChange}/>
+                </div>
+                 
                 <div className='show-all'>
-                    {sneakers.map(sneaker => <SneakerIndexItem key={sneaker.id} sneaker={sneaker} />)} 
+                    {sneakers.map(sneaker => <SneakerIndexItem key={sneaker.id} sneaker={sneaker} search={search}/>)} 
                 </div>
             </div>
         )
--- frontend/components/sneakers/sneakers_index_item.jsx
+++ frontend/components/sneakers/sneakers_index_item.jsx
@@ -2,7 +2,12 @@ import React from 'react';
 // import SneakerIndex from './sneakers_index/'
 import { Link } from 'react-router-dom';
 
-const SneakerItemIndex = ({ sneaker }) => {
+const SneakerItemIndex = ({ sneaker, search }) => {
+
+    if (search !== "" && sneaker.name.toLowerCase().indexOf(search.toLowerCase()) === -1) {
+        return null;
+    }
+
     return (
         <div className='sneakerItem'>
             <a href={`#/sneakers/${sneaker.id}`}><img src={sneaker.photoUrl}/></a>
