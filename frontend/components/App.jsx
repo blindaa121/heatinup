c92e67f3e2c0e179716119746fff7a279b774708
Bryan <blindaa121@gmail.com>
2020-06-10 20:54:21 -0700

    Clean up code

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -8,8 +8,6 @@ import SplashContainer from './splash/splash_container';
 import SneakerShowContainer from './sneaker_show/sneaker_show_container'
 import SneakersContainer from './sneakers/sneakers_container'
 import Styles from './styles/styles'
-import ListingsContainer from './listings/listing_container'
-import ListingDetails from './listings/listing_details'
 import ListingDetailsContainer from './listings/listing_details_container'
 import CollectionContainer1 from './collections/the_last_dance/sneakers_container'
 import CollectionContainer2 from './collections/fear_of_god/sneakers_container'
@@ -22,7 +20,6 @@ const App = () => (
         <Route path='/' component={NavBarContainer} />
         <Switch> 
             <Route exact path="/sneakers/:sneakerId/listings/:listingId" component={ListingDetailsContainer} />
-            {/* <Route path="/sneakers/:sneakerId/listings/" component={ListingsContainer} /> */}
             <Route path='/collections/thelastdance' component={CollectionContainer1} />
             <Route path='/collections/fog' component={CollectionContainer2} />
             <Route path='/collections/offwhite' component={CollectionContainer3} />
@@ -33,7 +30,6 @@ const App = () => (
             <AuthRoute path='/login' component={LogInFormContainer} />
             <Route exact path="/" component={SplashContainer} />
             <ProtectedRoute path='/cart' component={CartContainer} />
-            {/* <Route path='*' component={SplashContainer} /> */}
         </Switch>
         <Route path='/' component={Footer} />
     </div>
