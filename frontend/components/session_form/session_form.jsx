cc3d17e1e7283e2659333cba91fda9f9e0bfe03c
Bryan <blindaa121@gmail.com>
2020-06-20 23:37:05 -0700

    Fix login redirect bug when errors are present

--- frontend/components/session_form/session_form.jsx
+++ frontend/components/session_form/session_form.jsx
@@ -23,7 +23,10 @@ class SessionForm extends React.Component {
       e.preventDefault();
       const user = Object.assign({}, this.state);
       this.props.processForm(user);
-      this.props.history.goBack();
+      console.log(this.props.history)
+      if (!this.props.errors) {
+         this.props.history.goBack();
+      }
    }
 
    demoUser() {
