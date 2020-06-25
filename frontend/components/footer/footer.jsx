aaa304991e5c5f496226f8e1f5a00eb5761c084e
Bryan <blindaa121@gmail.com>
2020-06-24 17:01:49 -0700

    Add portfolio and angellist profile links to footer

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -5,8 +5,10 @@ const Footer = () => {
         <div className='footer-container'>
             <footer>
                 <div className='footer-links'>
+                    <a className='footer-icon' href="https://bryanlinda.com"><i className="fas fa-briefcase fa-3x"></i></a>
                     <a className='footer-icon' href="https://www.linkedin.com/in/bryan-linda-44389794/"><i className="fab fa-linkedin fa-3x"></i></a>
                     <a className='footer-icon' href="https://github.com/blindaa121/heatinup"><i className="fab fa-github fa-3x"></i></a>
+                    <a className='footer-icon' href="https://angel.co/u/bryan-linda"><i className="fab fa-angellist fa-3x"></i></a>
                 </div>    
             </footer>
         </div>
