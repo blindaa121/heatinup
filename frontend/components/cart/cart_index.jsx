7b0a87f57ff89f2967f8dafbc64733cf6938619e
Bryan <blindaa121@gmail.com>
2020-06-26 23:34:30 -0700

    Code cleanup

--- frontend/components/cart/cart_index.jsx
+++ frontend/components/cart/cart_index.jsx
@@ -19,36 +19,26 @@ class CartIndex extends React.Component {
         return (
         <div className='outer-sneakerComponent outer-cartComponent'>
             <div className='sneakerComponent'>
-
                 <div className='leftCart-pane'>
-
                     <div className='cart-items1'>
-
                         <div className='cart-items'>
                             <h1 className='cart-items-header'>Shopping Cart</h1>
                             <h3 className='total-items'>Total Items: {cartItems.length}</h3>
                         </div>
-
                         <div className='cart-items-containter'>
-                            {
-                                cartItems.map(cartItem => (<CartIndexItem cartItem={cartItem} deleteCartItem={deleteCartItem} key={cartItem.id} />))
-                            }
+                            { cartItems.map(cartItem => (<CartIndexItem cartItem={cartItem} deleteCartItem={deleteCartItem} key={cartItem.id} />))}
                         </div>
-
                     </div>
-
                 </div>
 
                 <div className='rightCart-pane'>
                     <div className='order-summary'>
                         <h1 className='order-summary-header'>Order Summary</h1>
                         <div className='order-shipping order-details'>
-                         {/* ship to */}
                             <span>Ship To</span>
                             <span>3960 Landmark St, Culver City</span>
                         </div>
                         <div className='order-card-info order-details'>
-                         {/* card info */}
                             <span>Payment</span>
                                 <span>
                                     <i className="payment fab fa-cc-paypal"></i>
@@ -62,24 +52,20 @@ class CartIndex extends React.Component {
                             <span>${subTotal}</span>
                         </div>
                         <div className='order-shipping order-details'>
-                         {/* shipping */}
                             <span>Shipping</span>
                             <span>$12</span>
                         </div>
                         <div className='order-total order-details'>
-                         {/* total */}
                             <span>Total</span>
                             <span>${subTotal + 12}</span>
                         </div>
                     </div>
-                    {/* <button onClick={() => this.handleClick()} className='buy-new-btn'>Buy New</button> */}
                     <div className='network-links'>
                             <a className='linked-in' href="https://www.linkedin.com/in/bryan-linda-44389794/">
                                 <div className='linked-in-btn'>
                                 LinkedIn
                                 </div>
                             </a>
-                        
                             <a className='github' href="https://github.com/blindaa121/heatinup">
                                 <div className='github-btn'>
                                     GitHub
