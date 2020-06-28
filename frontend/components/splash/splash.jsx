855f572726b5cd73ebd0623db4d5b042334f5fba
Bryan <blindaa121@gmail.com>
2020-06-28 11:03:12 -0700

    Removed unused import from splash

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import SneakerPanelContainer from '../sneaker_panel/sneaker_panel_container';
 import SneakerPanelContainer2 from '../sneaker_panel/fog_panel/sneaker_panel_container'
 import SneakerPanelContainer3 from '../sneaker_panel/offwhite_panel/sneaker_panel_container'
-// import NavBarContainer from ''
+
 class Splash extends React.Component {
     constructor(props) {
         super(props)
