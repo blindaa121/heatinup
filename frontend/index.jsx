1813f46dabf79ddc5279d74108d64081fb9dceb6
Bryan <blindaa121@gmail.com>
2020-07-31 20:22:12 -0700

    Revert "Remove testing on window"
    This reverts commit 489fd329987f72893c8048e96e4932cc5a7b1f71.
    
    Revert back to last bug free verison

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
+import { signin, signout, signup } from './actions/session_actions'
 import configureStore from './store/store';
 import Root from './components/root';
+import { fetchSneaker, fetchSneakers } from './actions/sneakers_actions';
+import { fetchListings, fetchListing } from './actions/listings_actions';
+import { fetchCart, addCartItem, deleteCartItem, updateCartItem } from './actions/cart_actions';
 
 document.addEventListener('DOMContentLoaded', () => {
     let store;
@@ -19,5 +23,22 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     const root = document.getElementById('root');
     ReactDOM.render(<Root store={store}/>, root)   
+    //TESTING
+    window.fetchListings = fetchListings
+    window.fetchListing = fetchListing
+    window.store = store 
+    window.getState = store.getState
+    window.dispatch = store.dispatch
+
+    // SNEAKER ACTIONS
+    window.fetchSneakers = fetchSneakers
+    window.fetchSneaker = fetchSneaker
+    window.signout = signout
+    
+    // CART ACTIONS
+    window.fetchCart = fetchCart 
+    window.addCartItem = addCartItem 
+    window.deleteCartItem = deleteCartItem
+    window.updateCartItem = updateCartItem
 });
 
